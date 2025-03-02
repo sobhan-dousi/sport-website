61065f10ae760119aed78d3b5b6c003f58a55caf
mr-ezak <ezaksaeed@gmail.com>
2025-03-02 21:00:02 +0330

    My JS tests

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const myname = "sobhan"
+let family = "Ezak"
+var age = 26
+
+let isRight = false
+let namesArray = ["ali", "hasan", "reza"]
+
+let myInformation = {
+  myName: "Saeed",
+  myLastname: "Ezak",
+  myAge: 30,
+}
+
+console.log(myInformation.myAge)
